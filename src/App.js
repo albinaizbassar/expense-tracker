ca1e6670cd096c16d4e3418c8698f969d2bf5718
bravelhino <saparov20003@mail.ru>
2021-08-17 15:44:22 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from "react";
+import Tracker from "./components/Tracker";
+import Login from "./components/Login";
 function App() {
+  const [user, setUser] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user ?
+          (
+            <Tracker setUser={setUser} />
+          ) :
+          (
+            <Login setUser={setUser}/>
+          )
+      }
     </div>
   );
 }
-
 export default App;
